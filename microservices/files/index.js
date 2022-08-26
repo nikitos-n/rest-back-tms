99f1864f676193f2fe5f4c7ddbf05da745281305
nikitos-n <Nikita27142914@yandex.ru>
2022-08-26 22:54:03 +0200

    init services

--- microservices/files/index.js
+++ microservices/files/index.js
@@ -5,13 +5,13 @@ const express = require('express')
 const path = require('path')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
-const authRouter = require('./routes/auth')
-const usersRouter = require('./routes/users')
-const petsRouter = require('./routes/pets')
-const errorHandler = require('./middlewares/errorHandler')
+// const authRouter = require('../../src/routes/auth')
+// const usersRouter = require('../../src/routes/users')
+// const petsRouter = require('../../src/routes/pets')
+// const errorHandler = require('../../src/middlewares/errorHandler')
 
-const { sequelize } = require('./services/db/models')
-const getCacheClient = require('./services/cache')
+// const { sequelize } = require('../../src/services/db/models')
+// const getCacheClient = require('../../src/services/cache')
 
 const { APP_PORT: PORT } = process.env
 
@@ -25,21 +25,21 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(async (req, res, next) => {
-  req.cacheClient = await getCacheClient()
-  next()
-})
+// app.use(async (req, res, next) => {
+//   req.cacheClient = await getCacheClient()
+//   next()
+// })
 
-app.use('/api', authRouter)
-app.use('/api', usersRouter)
-app.use('/api', petsRouter)
+// app.use('/api', authRouter)
+// app.use('/api', usersRouter)
+// app.use('/api', petsRouter)
 
-app.use(errorHandler)
+// app.use(errorHandler)
 
 async function main() {
   try {
-      await sequelize.sync({ force: false })
-      console.log('Connection has been established successfully.')
+      // await sequelize.sync({ force: false })
+      // console.log('Connection has been established successfully.')
       
       server.listen(PORT, () => {
         console.log(`Server is running on ${PORT} port`)
